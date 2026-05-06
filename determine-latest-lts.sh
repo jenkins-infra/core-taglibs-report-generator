b96328ae0fcf1e21e8c5a4a96d2a086ec283d557
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2026-05-06 21:01:17 +0200

    `jq` is already installed on all agents, its query is simple enough to be version independent

--- determine-latest-lts.sh
+++ determine-latest-lts.sh
@@ -4,13 +4,4 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-function die() {
-	echo "$(basename "$0"): $*" >&2
-	exit 1
-}
-
-curl --silent --fail --output jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 || die 'failed to download jq'
-chmod +x jq || die 'failed to make jq executable'
-curl --silent --fail 'https://repo.jenkins-ci.org/api/search/versions?g=org.jenkins-ci.main&a=jenkins-core&repos=releases&v=?.*.*' | ./jq --raw-output '.results[].version' | head -n 1
-
-exit 0
+curl --silent --fail 'https://repo.jenkins-ci.org/api/search/versions?g=org.jenkins-ci.main&a=jenkins-core&repos=releases&v=?.*.*' | jq --raw-output '.results[].version' | head -n 1
