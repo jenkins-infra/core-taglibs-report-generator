29dbe0ef3ed954550da7325cf7bfd03240a52652
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2026-05-06 20:45:28 +0200

    use `curl` instead of `wget` (not present in ci.jenkins.io's `maven-25` agent)

--- determine-latest-lts.sh
+++ determine-latest-lts.sh
@@ -9,7 +9,7 @@ function die() {
 	exit 1
 }
 
-wget -q -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 || die 'failed to download jq'
+curl --silent --fail --output jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 || die 'failed to download jq'
 chmod +x jq || die 'failed to make jq executable'
 curl --silent --fail 'https://repo.jenkins-ci.org/api/search/versions?g=org.jenkins-ci.main&a=jenkins-core&repos=releases&v=?.*.*' | ./jq --raw-output '.results[].version' | head -n 1
 
