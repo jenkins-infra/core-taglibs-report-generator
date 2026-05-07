659dd5e7efcb65f5f9844422ce5c08309a07dc60
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2026-05-07 09:41:09 +0200

    debug

--- determine-latest-lts.sh
+++ determine-latest-lts.sh
@@ -4,6 +4,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+set -x
+
 function die() {
 	echo "$(basename "$0"): $*" >&2
 	exit 1
@@ -11,6 +13,8 @@ function die() {
 
 curl --silent --fail --output jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 || die 'failed to download jq'
 chmod +x jq || die 'failed to make jq executable'
-curl --silent --fail 'https://repo.jenkins-ci.org/api/search/versions?g=org.jenkins-ci.main&a=jenkins-core&repos=releases&v=?.*.*' | ./jq --raw-output '.results[].version' | head -n 1
+results="$(curl --silent --fail 'https://repo.jenkins-ci.org/api/search/versions?g=org.jenkins-ci.main&a=jenkins-core&repos=releases&v=?.*.*')"
+versions="$(echo "${results}" | ./jq --raw-output '.results[].version')"
+echo "${versions}" | head -n 1
 
 exit 0
