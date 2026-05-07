0c050ffddbc04f2df67e232ea400056766c8e50e
Damien Duportal <damien.duportal@gmail.com>
2026-05-07 17:11:08 +0200

    Apply suggestion from @dduportal

--- determine-latest-lts.sh
+++ determine-latest-lts.sh
@@ -5,6 +5,6 @@ set -o nounset
 set -o pipefail
 
 # Fail fast if `jq` is missing
-command -v jq 2>/dev/null || { echo "$(basename "$0"): 'jq' command not found."; exit 1; }
+command -v jq 1>/dev/null  2>&1 || { echo "$(basename "$0"): 'jq' command not found."; exit 1; }
 
 curl --silent --fail 'https://repo.jenkins-ci.org/api/search/versions?g=org.jenkins-ci.main&a=jenkins-core&repos=releases&v=?.*.*' | jq --raw-output '.results[].version' | head -n 1
