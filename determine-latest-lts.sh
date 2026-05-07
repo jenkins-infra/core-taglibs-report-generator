c59b5c6f466dec4532c7ded0287904053a80522c
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2026-05-07 16:39:36 +0200

    Update determine-latest-lts.sh
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>

--- determine-latest-lts.sh
+++ determine-latest-lts.sh
@@ -4,4 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Fail fast if `jq` is missing
+command -v jq 2>/dev/null || { echo "$(basename "$0"): 'jq' command not found."; exit 1; }
+
 curl --silent --fail 'https://repo.jenkins-ci.org/api/search/versions?g=org.jenkins-ci.main&a=jenkins-core&repos=releases&v=?.*.*' | jq --raw-output '.results[].version' | head -n 1
